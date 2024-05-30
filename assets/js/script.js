4e091b6f84d8a943891141376dbafa8296f7d13f
sout1235 <167926611+sout1235@users.noreply.github.com>
2024-05-30 19:49:25 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,25 @@ if (savedTheme) {
     theme.href = savedTheme; 
 } 
 switchMode.onclick = function(){
+  const wave = document.createElement('div');
+  wave.classList.add('wave');
+  document.body.appendChild(wave);
+  const rect = switchMode.getBoundingClientRect();
+  wave.style.left = (rect.left + rect.width/2) + 'px';
+  wave.style.top = (rect.top + rect.height/2) + 'px';
+  wave.addEventListener('animationend', function() {
+      wave.remove();
+  });
+  setTimeout(function() {
     if(theme.getAttribute("href") == "assets/css/web.css"){
         theme.href = "assets/css/secondVar.css";
     }
     else{
         theme.href = "assets/css/web.css";
     }
-    localStorage.setItem('theme', theme.href); 
+    localStorage.setItem('theme', theme.href);
+  }, 250);
 }
-
 window.onload = function(){
     let preloader = document.getElementById ('preloader');
     preloader.classList.add('hide-preloader');
@@ -21,48 +31,38 @@ window.onload = function(){
         preloader.classList.add('preloader-hidden');
     },950)
 }
-
 function changeFontSizeUp() {
-  // Получаем все элементы без указания классов
-  var allP = document.querySelectorAll('p');
-  var allA = document.querySelectorAll('a');
-  var allLi = document.querySelectorAll('.header__menu li');
-  // Устанавливаем новый размер текста и уменьшаем margin для каждого элемента
-  allP.forEach(changeSizeAndMargin(2));
-  allA.forEach(changeSizeAndMargin(2));
-  allLi.forEach(changeMargin(-2)); // Уменьшаем margin для элементов списка меню
-}
-
-function changeFontSizeDown() {
-  // Получаем все элементы без указания классов
-  var allP = document.querySelectorAll('p');
-  var allA = document.querySelectorAll('a');
-  var allLi = document.querySelectorAll('.header__menu li');
-  // Устанавливаем новый размер текста и увеличиваем margin для каждого элемента
-  allP.forEach(changeSizeAndMargin(-2));
-  allA.forEach(changeSizeAndMargin(-2));
-  allLi.forEach(changeMargin(2)); // Увеличиваем margin для элементов списка меню
-}
-
-// Вспомогательная функция для изменения размера шрифта и margin
-function changeSizeAndMargin(delta) {
-  return function(element) {
+    var allP = document.querySelectorAll('p');
+    var allA = document.querySelectorAll('a');
+    allP.forEach(function(element) {
       var currentSize = parseFloat(window.getComputedStyle(element).fontSize);
-      var newSize = currentSize + delta;
+      var newSize = currentSize + 2;
       element.style.fontSize = newSize + "px";
-      
-      // Добавляем изменение margin для элементов 'p' и 'a'
-      var currentMargin = parseFloat(window.getComputedStyle(element).marginRight);
-      var newMargin = currentMargin - delta; // Обратите внимание на знак минус для уменьшения margin
-      element.style.marginRight = newMargin + "px";
-  };
-}
-
-// Вспомогательная функция для изменения margin
-function changeMargin(delta) {
-  return function(element) {
-      var currentMargin = parseFloat(window.getComputedStyle(element).marginRight);
-      var newMargin = currentMargin + delta;
-      element.style.marginRight = newMargin + "px";
-  };
-}  
+    });
+    allA.forEach(function(element) {
+        var currentSize = parseFloat(window.getComputedStyle(element).fontSize);
+        var newSize = currentSize + 2;
+        element.style.fontSize = newSize + "px";
+      });
+  }
+  function changeFontSizeDown() {
+    var allP = document.querySelectorAll('p');
+    var allA = document.querySelectorAll('a');
+    var allH1 = document.querySelectorAll('h1');
+    allP.forEach(function(element) {
+      var currentSize = parseFloat(window.getComputedStyle(element).fontSize);
+      var newSize = currentSize - 2;
+      element.style.fontSize = newSize + "px";
+    });
+    allA.forEach(function(element) {
+        var currentSize = parseFloat(window.getComputedStyle(element).fontSize);
+        var newSize = currentSize - 2;
+        element.style.fontSize = newSize + "px";
+      });
+    allH1.forEach(function(element) {
+        var currentSize = parseFloat(window.getComputedStyle(element).fontSize);
+        var newSize = currentSize - 2;
+        element.style.fontSize = newSize + "px";
+      });  
+  }
+    
